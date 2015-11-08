df35d354fe4aceeaadf269e25b10310868d2fac6
Lydia Auch <lidauch@gmail.com>
2015-11-07 21:10:01 -0500

    In Progress site

--- src/js/main.js
+++ src/js/main.js
@@ -10,35 +10,3 @@
 		});
 	});
 }
-
-// Div scrolling
-
-$(window).scroll(function(){
-    $("#introtext").css("top", Math.max(0, 250 - $(this).scrollTop()));
-});
-// $(window).scroll(function(){
-//    if ($(window).scrollTop() >= 10) {  // change target to number
-//       $("#el").css('position', 'absolute');
-//    }
-// });
-
-// $.fn.followTo = function (pos) {
-//     var $this = this,
-//         $window = $(window);
- 
-//     $window.scroll(function (e) {
-//         if ($window.scrollTop() > pos) {
-//             $this.css({
-//                 position: 'absolute',
-//                 top: pos
-//             });
-//         } else {
-//             $this.css({
-//                 position: 'fixed',
-//                 top: 0
-//             });
-//         }
-//     });
-// };
- 
-// $('#topwrapper').followTo(250);
