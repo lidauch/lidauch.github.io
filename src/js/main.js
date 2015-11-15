3c435ea9a484334ebb22aa6c88d2bc0a2c5a0b5b
Lydia Auch <lidauch@gmail.com>
2015-11-14 21:26:07 -0500

    Update pages 1 and 2 and add footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,30 @@
-// Parallax Scrolling
-(document).ready(function()) {
-	var $window = $(window);
-	$('section[data-type="background"]').each(function(){
-		var $bgobj = $(this);
-		$(window).scroll(function() {
-			var yPos = -($window.scrollTop() / $bgobj.data('speed'));
-			var coords = yPos + 'px';
-			$bgobj.css({ backgroundPosition: coords });
-		});
-	});
-}
+// // Parallax Scrolling
+// $(document).ready(function() {
+// 	var $window = $(window);
+// 	$('section[data-type="background"]').each(function(){
+// 		var $bgobj = $(this);
+// 		$(window).scroll(function() {
+// 			var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+// 			var coords = yPos + 'px';
+// 			$bgobj.css({ backgroundPosition: coords });
+// 		});
+// 	});
+// });
+
+// Scroll to bottom
+$(document).ready(function() {
+    $('#scrollDown').click(function(){
+        $('html, body').animate({scrollTop:$(document).height()}, 'slow');
+        return false;
+    });
+});
+
+// Hide text on scroll
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1000) {
+        $('#introtext').hide();
+    }
+    if ($(this).scrollTop() < 1000) {
+        $('#introtext').show();
+    }
+});
