7850535fcf44ed7679e04a13e5a3820cd07c55fb
Lydia Auch <lidauch@gmail.com>
2015-12-03 23:25:28 -0500

    update gruntfile and minify js

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,13 +9,34 @@ module.exports = function(grunt) {
 			}
 		}, 
 		watch: {
+			files: ['<%= jshint.files %>'],
+			tasks: 'jshint',
 			css: {
 				files: '**/*.scss', 
 				tasks: ['sass']
 			}
+		},
+		uglify: {
+			dist: {
+				files: {
+					'dist/<%= pkg.name %>.min.js' : 'src/js/main.js'
+				}
+			}
+		}, 
+		jshint: {
+			files: ['Gruntfile.js', 'src/js/main.js'],
+			options: {
+				globals: {
+					jQuery: true,
+				}
+			}
 		}
+
 	});
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default', ['watch']);
-}
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.registerTask('default', ['uglify', 'jshint']);
+	grunt.registerTask('development', ['uglify', 'watch']);
+};
