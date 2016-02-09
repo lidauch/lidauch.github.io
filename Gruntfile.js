5f6786930b53714b10d67e5878fd18d4b29bfee6
Lydia Auch <lidauch@gmail.com>
2016-02-08 21:59:42 -0500

    about me page and updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,13 +4,23 @@ module.exports = function(grunt) {
 		sass: {
 			dist: {
 				files: {
-					'src/css/global.css' : 'src/css/scss/global.scss' 
+					'src/css/global.css' : 'src/css/scss/global.scss',
+					'src/css/subpage.css' : 'src/css/scss/subpage.scss' 
 				}
 			}
 		}, 
+		cssmin: {
+			dist: {
+				files: {
+					'dist/css/style.min.css' : ['src/css/global.css']
+				}
+			}
+		},
+		clean: ["dist/css/*"],
+		// issue here with cssmin when sass writes to css on livereload
 		watch: {
 			files: ['<%= jshint.files %>'],
-			tasks: 'jshint',
+			tasks: ['jshint', 'clean', 'cssmin'],
 			css: {
 				files: '**/*.scss', 
 				tasks: ['sass']
@@ -30,13 +40,24 @@ module.exports = function(grunt) {
 					jQuery: true,
 				}
 			}
+		},
+		imagemin: {
+			files: [{
+				expand: true,
+				cwd: 'src/images', 
+				src: ['**/*.{png,jpg,gif}'],
+				dest: 'dist/'
+			}]
 		}
 
 	});
 	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.registerTask('default', ['uglify', 'jshint']);
-	grunt.registerTask('development', ['uglify', 'watch']);
+	grunt.registerTask('development', ['uglify', 'clean', 'cssmin', 'watch']);
 };
